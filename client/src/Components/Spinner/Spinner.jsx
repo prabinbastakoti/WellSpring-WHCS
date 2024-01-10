442e0362ec098968b388aff0083b307a05348c45
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-01-10 20:28:05 +0545

    ripple loader

--- client/src/Components/Spinner/Spinner.jsx
+++ client/src/Components/Spinner/Spinner.jsx
@@ -3,17 +3,7 @@ import "./Spinner.css";
 export default function Spinner() {
   return (
     <>
-      <div class="lds-spinner">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div class="lds-ripple">
         <div></div>
         <div></div>
       </div>
