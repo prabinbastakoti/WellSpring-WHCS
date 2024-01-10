0bc97bef042a23d5cec8f9ff5aafcadb13c926aa
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-01-10 19:49:33 +0545

    setup prettier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,58 @@
-import background from './assets/19836.jpg'
-import logo from './assets/WellSpring.svg'
+import background from "./assets/19836.jpg";
+import logo from "./assets/WellSpring.svg";
 
 function App() {
-
   return (
     <>
-    <div className="h-screen flex bg-[#FAFAFA] relative">
-      <p className='absolute bottom-8 left-20 text-xs text-gray-400 font-serif lowercase'><a href="https://www.freepik.com/free-vector/medics-working-charts_4950249.htm#query=hospital%20management&position=0&from_view=keyword&track=ais&uuid=1eb5c821-c821-4428-8f49-83c3147d77d0">* Image by katemangostar</a> on Freepik</p>
-        <img src={background} className='w-3/5 object-cover '/>
+      <div className="h-screen flex bg-[#FAFAFA] relative">
+        <p className="absolute bottom-8 left-20 text-xs text-gray-400 font-serif lowercase">
+          <a href="https://www.freepik.com/free-vector/medics-working-charts_4950249.htm#query=hospital%20management&position=0&from_view=keyword&track=ais&uuid=1eb5c821-c821-4428-8f49-83c3147d77d0">
+            * Image by katemangostar
+          </a>{" "}
+          on Freepik
+        </p>
+        <img src={background} className="w-3/5 object-cover " />
 
-      <div className="flex-1 my-auto pr-20">
-          <img src={logo} className='mb-8 w-60 h-60 mx-auto'/>
-        <form className='flex flex-col items-center gap-6'>
-        <div class="relative w-3/5">
-    <input type="text" id="username" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-    <label for="username" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Username</label>
-</div>
-<div class="relative w-3/5">
-    <input type="password" id="password" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-    <label for="password" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Password</label>
-</div>
-          
-          <button className='bg-blue-500 my-3 py-2 px-8 rounded-md outline-none border-none text-white text-base text-center hover:brightness-105'>Login</button>
+        <div className="flex-1 my-auto pr-20">
+          <img src={logo} className="mb-8 w-60 h-60 mx-auto" />
+          <form className="flex flex-col items-center gap-6">
+            <div class="relative w-3/5">
+              <input
+                type="text"
+                id="username"
+                class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+              />
+              <label
+                for="username"
+                class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+              >
+                Username
+              </label>
+            </div>
+            <div class="relative w-3/5">
+              <input
+                type="password"
+                id="password"
+                class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+              />
+              <label
+                for="password"
+                class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+              >
+                Password
+              </label>
+            </div>
+
+            <button className="bg-blue-500 my-3 py-2 px-8 rounded-md outline-none border-none text-white text-base text-center hover:brightness-105">
+              Login
+            </button>
           </form>
-          
-          </div>
-    </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: { colors: {
-      primary: '#F5385D',
+    extend: {
+      colors: {
+        primary: "#F5385D",
+      },
     },
-    backgroundImage: { 'background': "url('/src/assets/414.jpg')" }
-  },
   },
   plugins: [],
-}
-
+};
